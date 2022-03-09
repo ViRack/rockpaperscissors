b9571a966c7fff2e593fef37889e8d94d80202f5
ViRack <vincentm_2@hotmail.com>
2022-03-09 01:31:46 -0800

    Compleed structure and logic. Finishing up some images and will be adding an about page.

--- js/game.js
+++ js/game.js
@@ -16,63 +16,79 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
+ }
 
 function roshambo(userChoice) {
-    console.log("test");
+    console.log("inside roshambo");
 
-
-    let aiChoice = getRandomInt(1, 4);
+	let userImg 	= document.getElementById('user-choice-image');
+	let aiImg 		= document.getElementById('ai-choice-image');
+    let aiChoice 	= getRandomInt(1, 4);
     console.log(aiChoice);
 
     if (userChoice === ROCK) {
+		userImg.src = '../images/rock.svg';
         if (aiChoice === ROCK) {
-            document.getElementById("who-won").innerHTML = 'hello';
+            document.getElementById("who-won").innerHTML = 'Draw!';
+			aiImg.src = '../images/rock.svg';
             console.log("in rock: rock");
         }
         if (aiChoice === PAPER) {
-            document.getElementById("who-won").innerHTML = 'hello';
+			aiScore++;
+            document.getElementById("who-won").innerHTML = 'Computer Wins!';
+			aiImg.src = '../images/paper.svg';
             console.log("in rock: paper");
         }
         if (aiChoice === SCISSORS) {
             userScore++;
-            document.getElementById("who-won").innerHTML = 'hello';
+            document.getElementById("who-won").innerHTML = 'You Win!';
+			aiImg.src = '../images/scissors.svg';
             console.log("in rock: scissors");
         }
     }
     
     if (userChoice === PAPER) {
+		userImg.src = '../images/paper.svg';
         if (aiChoice === ROCK) {
-            aiScore++;
-            document.getElementById("who-won").innerHTML = 'hello';
+            userScore++;
+            document.getElementById("who-won").innerHTML = 'You Win!';
+			aiImg.src = '../images/rock.svg';
             console.log("in paper: rock");
         }
         if (aiChoice === PAPER) {
-            document.getElementById("who-won").innerHTML = 'hello';
-            console.log("in paper: paper");
+            document.getElementById("who-won").innerHTML = 'Draw!';
+			aiImg.src = '../images/paper.svg';
+			console.log("in paper: paper");
         }
         if (aiChoice === SCISSORS) {
             aiScore++;
-            document.getElementById("who-won").innerHTML = 'hello';
+            document.getElementById("who-won").innerHTML = 'Computer Wins!';
+			aiImg.src = '../images/scissors.svg';
             console.log("in paper: scissors");
         }
     }
 
     if (userChoice === SCISSORS) {
+		userImg.src = '../images/scissors.svg';
         if (aiChoice === ROCK) {
             aiScore++;
-            document.getElementById("who-won").innerHTML = 'hello';
+            document.getElementById("who-won").innerHTML = 'Computer Wins!';
+			aiImg.src = '../images/rock.svg';
             console.log("in scissors: rock");
         }
         if (aiChoice === PAPER) {
-            aiScore++;
-            document.getElementById("who-won").innerHTML = 'hello';
-            console.log("in scissors: paper");
+            userScore++;
+            document.getElementById("who-won").innerHTML = 'You Win!';
+			aiImg.src = '../images/paper.svg';
+			console.log("in scissors: paper");
         }
         if (aiChoice === SCISSORS) {
-            document.getElementById("who-won").innerHTML = 'hello';
-            console.log("in scissors: scissors");
+            document.getElementById("who-won").innerHTML = 'Draw!';
+			aiImg.src = '../images/scissors.svg';
+			console.log("in scissors: scissors");
         }
     }
-
+	
+	document.getElementById("user-score").innerHTML = `Your Score is: ${userScore}`;
+	document.getElementById("ai-score").innerHTML = `Computer Score is: ${aiScore}`;
 }
